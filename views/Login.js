2596ca047180104b2e3b0d6b5b159b6cc838c29d
Luihkiin <luigipereirasouza@gmail.com>
2022-09-10 15:23:24 -0300

    Revert "Erro Unable to resolve module HTTP"
    This reverts commit 56d1ee8f62581493454fba582c5ca383c8da776c.

--- views/Login.js
+++ views/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, Component } from 'react';
+import React, { useEffect, useReducer, Component } from 'react';
 import { Alert, Text, View, TouchableOpacity, Image, TextInput } from "react-native";
 import { estilo } from '../assets/css/Css.js'
 import { LinearGradient } from "expo-linear-gradient";
@@ -6,80 +6,68 @@ import { useFonts } from 'expo-font';
 import Cadastro from './Cadastro';
 import Feather from 'react-native-vector-icons/Feather';
 import ElementosLogin from '../assets/components/ElementosLogin.js';
-import { response } from 'express';
-//import AsyncStorage from '@react-native-async-storage';
+import { useState } from 'react';
+
+class Construtor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      CPF: '',
+      PWD: '',
+      check_textInputChange: false,
+      secureTextEntry: true,
+    };
+  }
+}
+
+function InsertRecord() {
+  var CPF = this.state.CPF;
+  var PWD = this.state.PWD;
+
+  if ((CPF.length == 0) || PWD.length == 0) {
+    Alert.alert("Campos faltando!");
+  } else {
+    /*var APIURL = "http://localhost:80/Entrar/login.php"; //Utilizar este diretório dentro de HTDOCS (Retirar arquivos .php antigos)
+
+    var headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'appplication/json'
+    };*/
+
+    Alert.alert("Campos preenchidos!");
+  }
+  /*var Dados = {
+    CPF: CPF,
+    PWD: PWD,
+  };
+
+  //Função Fetch
+  fetch(APIURL, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(Dados) //Converte Dados para JSON
+  })
+    .then((Response) => Response.json())
+    .then((Response) => {
+      alert(Response[0].Message)
+      if (Response[0].Message == "Sucesso") {
+        console.log("true")
+        this.props.navigation.navigate(Cadastro); //Navega para a Tela Inicial (COM SERVIÇOS)
+      }
+      console.log(Date);
+    })
+    .catch((error) => {
+      console.error("Erro encontrado" + error);
+    })
+}*/
+}
+
 
 export default function Login({ navigation }) {
-
-  //Declaração de variáveis
-  const [CPF, setCPF] = useState('');
-  const [PWD, setPWD] = useState('');
 
   const [fontsLoaded] = useFonts({
     'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
   });
-
-  //Função para Gravar Dados  
-  function GravarDados() {
-
-    if ((CPF == '') || PWD == '') {
-      Alert.alert('Campos Faltando');
-    } else {
-      /*var APIURL = "http://localhost:80/hap/login.php"; //Utilizar este diretório dentro de HTDOCS (Retirar arquivos .php antigos)
-
-      var headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'appplication/json'
-      };
-      Alert.alert("Campos Preenchidos");*/
-
-      //Função que Faz Login
-      async function FazerLogin(){
-        let reqs = await fetch('http://localhost:3000/login',{
-          method: 'POST',
-          headers:{
-              'Accept':'application/json',
-              'Content-Type':'application/json'
-          },
-          body: JSON.stringify({
-              cpf: CPF,
-              senha: PWD
-          })
-      });
-      let json=await response.json();
-      if(json === 'error'){
-        setDisplay('flex');
-        setTimeout(()=>{
-          setDisplay('none');
-        },5000);
-      }
-      }
-    }
-    var Dados = {
-      CPF: CPF,
-      PWD: PWD,
-    };
-
-    //Função Fetch
-    /*fetch(APIURL, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify(Dados) //Converte Dados para JSON
-    })
-      .then((Response) => Response.json())
-      .then((Response) => {
-        alert(Response[0].Message)
-        if (Response[0].Message == "Sucesso") {
-          console.log("true")
-          this.props.navigation.navigate(Cadastro); //Navega para a Tela Inicial (COM SERVIÇOS)
-        }
-        console.log(Date);
-      })
-      .catch((error) => {
-        console.error("Erro encontrado" + error);
-      })*/
-
-  }
 
   function Teste() {
     Alert.alert('Mensagem de Teste', 'Funcionou!!!')
@@ -98,24 +86,24 @@ export default function Login({ navigation }) {
         <View style={estilo.informacao}>
           <Text style={estilo.texto}
           >
-            CPF: {CPF}
+            CPF
           </Text>
           <View style={estilo.caixa}>
             <TextInput
               placeholder="Insira seu CPF aqui"
               keyboardType="numeric"
-              onChangeText={(text) => setCPF(text)}
+              onChangeText={CPF => this.setState({ CPF })}
             >
             </TextInput>
           </View>
           <Text style={estilo.texto}
           >
-            Senha: {PWD}
+            Senha
           </Text>
           <View style={estilo.caixa}>
             <TextInput
               placeholder="Insira sua senha aqui"
-              onChangeText={(text) => setPWD(text)}
+              onChangeText={PWD => this.setState({ PWD })}
               /*secureTextEntry={ElementosLogin.state.secureTextEntry ? true : false}
   onChangeText={ PWD => this.state({PWD})}*/>
 
@@ -139,7 +127,7 @@ export default function Login({ navigation }) {
             //onPress={ElementosLogin.InsertRecord}>
 
             //Teste com o Fetch dentro do onPress
-            onPress={GravarDados}>
+            onPress={InsertRecord}>
             <Text style={estilo.clicavel}>
               Entrar
             </Text>
